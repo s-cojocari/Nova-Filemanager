3255fe4b1502fc028b619c0cb00bc6c9c9f8af66
Emil Ravnsholt <ero@peytz.dk>
2023-05-01 13:31:51 +0200

    wip: fixed disk to s3

--- src/Http/Services/GetFiles.php
+++ src/Http/Services/GetFiles.php
@@ -196,7 +196,7 @@ trait GetFiles
      */
     public function setRelativePath($folder)
     {
-        dd(get_class_methods( Storage::disk( config('filesystems.default') )->getAdapter() ) );
+        dd(get_class_methods( Storage::disk( 's3' )->getAdapter() ) );
         // $defaultPath = $this->storage->getDriver()->getAdapter()->getPathPrefix();
         $defaultPath = Storage::disk(config('filesystems.default'))->getAdapter()->getPathPrefix();
 
