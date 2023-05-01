a86140cdad2cab0262f18f51a37f2855f771ed5e
Emil Ravnsholt <ero@peytz.dk>
2023-05-01 12:55:14 +0200

    wip: added die dump for test purpose

--- src/Http/Services/GetFiles.php
+++ src/Http/Services/GetFiles.php
@@ -465,6 +465,7 @@ trait GetFiles
      */
     public function getPaths($currentFolder)
     {
+        dd(Storage::disk(config('filesystems.default'))->getAdapter());
         // $defaultPath = $this->cleanSlashes($this->storage->getDriver()->getAdapter()->getPathPrefix());
         $defaultPath = $this->cleanSlashes(Storage::disk(config('filesystems.default'))->getAdapter()->getPathPrefix());
         $currentPath = $this->cleanSlashes($this->storage->path($currentFolder));
